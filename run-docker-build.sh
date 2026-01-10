12c4aa4a1a31ce966065eed8751c39633e2bd62b
Nizo Priskorn <68460690+dpriskorn@users.noreply.github.com>
2026-01-10 17:46:03 +0100

    chore: Update docker scripts

--- run-docker-build.sh
+++ run-docker-build.sh
@@ -4,5 +4,6 @@ set -e
 ./run-linters.sh
 docker compose --file docker/docker-compose.yml down --remove-orphans -t 0 -v 
 nice -20 docker compose --file docker/docker-compose.yml build
-docker compose up --build -d
+docker compose build --progress=plain
+docker compose up -d
 docker logs -f integration-tests
