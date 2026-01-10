e8f29a801b9fcaaab70b9ee9a11c05ed16ec3b88
Nizo Priskorn <68460690+dpriskorn@users.noreply.github.com>
2026-01-10 19:32:18 +0100

    fix: Fix docker warning

--- run-docker-build.sh
+++ run-docker-build.sh
@@ -5,6 +5,6 @@ set -e
 ./run-linters.sh
 docker compose --file docker/docker-compose.yml down --remove-orphans -t 0 -v 
 nice -20 docker compose --file docker/docker-compose.yml build
-docker compose build --progress=plain
+docker compose --progress=plain build 
 docker compose up -d
 docker logs -f integration-tests
