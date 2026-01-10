97118d96a82cfe251dec45781ae3ef9b71b9933b
Nizo Priskorn <68460690+dpriskorn@users.noreply.github.com>
2026-01-10 20:18:14 +0100

    chore: Add helper script

---
+++ scripts/download/stream_lexeme_json_gz.py
@@ -0,0 +1,13 @@
+import requests
+import gzip
+
+url = "https://dumps.wikimedia.org/wikidatawiki/entities/latest-lexemes.json.gz"
+
+with requests.get(url, stream=True) as r:
+    r.raise_for_status()
+    with gzip.GzipFile(fileobj=r.raw) as f:
+        # Skip first line
+        next(f)
+        # Read second line
+        second_line = next(f).decode("utf-8", errors="replace").strip()
+        print(second_line)
