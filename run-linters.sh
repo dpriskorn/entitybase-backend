f8589611b2a6e8ec3c3644132d2a53fc4e30b28f
Nizo Priskorn <68460690+dpriskorn@users.noreply.github.com>
2026-01-11 11:48:14 +0100

    chore: Update linter conf

--- run-linters.sh
+++ run-linters.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -e
 
-./scripts/custom_docstring_linter.py
-./scripts/basemodel_linter.py
+#./scripts/custom_docstring_linter.py
+#./scripts/basemodel_linter.py
 ./run-ruff.sh
 ./run-mypy.sh
-#./run-vulture.sh
+./run-vulture.sh
 ./run-radon.py
