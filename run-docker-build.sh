56acff0edc8713e8dea68d28514610892e904d5f
Nizo Priskorn <68460690+dpriskorn@users.noreply.github.com>
2026-01-14 12:29:25 +0100

    fix: add docker volume prune to ensure clean DB state before builds

--- run-docker-build.sh
+++ run-docker-build.sh
@@ -4,6 +4,7 @@ set -e
 # ./update-docs.sh
 ./run-linters.sh
 docker compose --file docker/docker-compose.yml down --remove-orphans -t 0 -v 
+docker volume prune -f
 nice -20 docker compose --file docker/docker-compose.yml build --quiet
 docker compose --file docker/docker-compose.yml --progress=plain build
 time docker compose --file docker/docker-compose.yml up -d
