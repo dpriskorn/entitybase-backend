1f950352e14488cf6ccc4e10c6917812b0d6b136
Nizo Priskorn <68460690+dpriskorn@users.noreply.github.com>
2026-01-14 12:36:56 +0100

    fix: correct redirection syntax in docker commands

--- run-docker-build.sh
+++ run-docker-build.sh
@@ -4,9 +4,8 @@ set -e
 # ./update-docs.sh
 ./run-linters.sh
 docker compose --file docker/docker-compose.yml down --remove-orphans -t 0 -v 
-docker volume prune -f
-nice -20 docker compose --file docker/docker-compose.yml build --quiet
-docker compose --file docker/docker-compose.yml --progress=plain build
+docker volume prune -f >/dev/null 2>&1
+nice -20 docker compose --file docker/docker-compose.yml build >/dev/null 2>&1
 time docker compose --file docker/docker-compose.yml up -d
 # docker logs -f idworker
 # docker logs -f tests
