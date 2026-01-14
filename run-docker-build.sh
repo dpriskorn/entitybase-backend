e4c327d763239dc168f78e1a672cfcef9c2541ba
Nizo Priskorn <68460690+dpriskorn@users.noreply.github.com>
2026-01-14 18:09:52 +0100

    chore: Fix script

--- run-docker-build.sh
+++ run-docker-build.sh
@@ -4,8 +4,13 @@ set -e
 docker compose --file docker/docker-compose.yml down --remove-orphans -t 0 -v #>/dev/null 2>&1
 docker volume prune -f #>/dev/null 2>&1
 docker volume ls
+docker ps -aq | xargs -r docker rm -f
+docker container ls
 # ./run-linters.sh
-nice -20 docker compose --file docker/docker-compose.yml build >/dev/null 2>&1
+# with cache
+nice -20 docker compose --file docker/docker-compose.yml build # >/dev/null 2>&1
+# no cache
+# nice -20 docker compose --file docker/docker-compose.yml build --no-cache # >/dev/null 2>&1
 time docker compose --file docker/docker-compose.yml up -d
 # docker logs -f idworker
 docker logs -f tests
