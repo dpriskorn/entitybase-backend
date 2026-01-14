4a8395edae64153eb946d37eb453a85a6f596537
Nizo Priskorn <68460690+dpriskorn@users.noreply.github.com>
2026-01-14 12:38:22 +0100

    chore: Hide teardown output

--- run-docker-build.sh
+++ run-docker-build.sh
@@ -3,7 +3,7 @@ set -e
 
 # ./update-docs.sh
 ./run-linters.sh
-docker compose --file docker/docker-compose.yml down --remove-orphans -t 0 -v 
+docker compose --file docker/docker-compose.yml down --remove-orphans -t 0 -v >/dev/null 2>&1
 docker volume prune -f >/dev/null 2>&1
 nice -20 docker compose --file docker/docker-compose.yml build >/dev/null 2>&1
 time docker compose --file docker/docker-compose.yml up -d
