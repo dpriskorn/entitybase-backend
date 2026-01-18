0753e60d474a6ec147977f3b43f09b256d24656f
Nizo Priskorn <68460690+dpriskorn@users.noreply.github.com>
2026-01-18 13:45:43 +0100

    chore: Fix script

--- run-mypy.sh
+++ run-mypy.sh
@@ -3,4 +3,10 @@ set -euo pipefail
 
 source .venv/bin/activate
 
-mypy --cache-dir .mypy_cache --sqlite-cache --explicit-package-bases --disable-error-code=union-attr --disable-error-code=return-value src/
+mypy \
+  --cache-dir .mypy_cache \
+  --sqlite-cache \
+  --explicit-package-bases \
+  --disable-error-code=union-attr \
+  --disable-error-code=return-value \
+  src/
