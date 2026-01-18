45e6fd8a78032b5c0e624810277060121946f8bc
Nizo Priskorn <68460690+dpriskorn@users.noreply.github.com>
2026-01-18 10:19:46 +0100

    chore: Fix script

--- run-mypy.sh
+++ run-mypy.sh
@@ -4,9 +4,3 @@ set -euo pipefail
 source .venv/bin/activate
 
 mypy --cache-dir .mypy_cache --sqlite-cache --explicit-package-bases src/
-
-# slow per file run
-#for file in $(find src tests scripts -name "*.py"); do
-#    echo "Checking $file"
-#    mypy --pretty --show-error-codes --no-error-summary "$file" && continue || exit 1
-#done
