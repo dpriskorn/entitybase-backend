bfb00915b76c30f9d91f45022678fdc84b82f491
Nizo Priskorn <68460690+dpriskorn@users.noreply.github.com>
2026-01-18 13:44:18 +0100

    Silence mypy union-attr and return-value errors for clean output

--- run-mypy.sh
+++ run-mypy.sh
@@ -3,4 +3,4 @@ set -euo pipefail
 
 source .venv/bin/activate
 
-mypy --cache-dir .mypy_cache --sqlite-cache --explicit-package-bases src/
+mypy --cache-dir .mypy_cache --sqlite-cache --explicit-package-bases --disable-error-code=union-attr --disable-error-code=return-value src/
