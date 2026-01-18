5c05f2049bd9579e31309ff61df8969fd6209ccf
Nizo Priskorn <68460690+dpriskorn@users.noreply.github.com>
2026-01-18 14:29:46 +0100

    Change lexemes endpoints tag from 'lexemes' to 'lexeme'

--- src/models/rest_api/entitybase/versions/v1/__init__.py
+++ src/models/rest_api/entitybase/versions/v1/__init__.py
@@ -21,7 +21,7 @@ v1_router = APIRouter()
 # Include sub-routers
 v1_router.include_router(entities.router, tags=["entities"])
 v1_router.include_router(items.router, tags=["items"])
-v1_router.include_router(lexemes.router, tags=["lexemes"])
+v1_router.include_router(lexemes.router, tags=["lexeme"])
 v1_router.include_router(properties.router, tags=["properties"])
 v1_router.include_router(statements.router, tags=["statements"])
 v1_router.include_router(admin.admin_router, tags=["admin"])
