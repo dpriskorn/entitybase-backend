1d0bf2eb431d5a17d6b55633edb6c1efb67e9d4d
Nizo Priskorn <68460690+dpriskorn@users.noreply.github.com>
2026-01-27 20:43:53 +0100

    chore: Add script and remove script

---
+++ run-single-test.sh
@@ -0,0 +1,18 @@
+set -e
+
+if [ "$(docker ps -q | wc -l)" -gt 0 ]; then
+  echo "Containers are running"
+else
+  echo "No containers are running, run ./run-api-local.sh before this"
+  exit 1
+fi
+
+export VITESS_HOST=localhost
+export VITESS_PORT=3306
+export VITESS_DATABASE=entitybase
+export VITESS_USER=root
+export VITESS_PASSWORD=""
+
+echo "Running single test"
+
+pytest $1
