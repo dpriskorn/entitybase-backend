e55ebeb51b26569a22037f15adc7fbdf89b66e75
Nizo Priskorn <68460690+dpriskorn@users.noreply.github.com>
2026-01-28 17:38:22 +0100

    chore: Fix test scripts

---
+++ run-e2e-tests.sh
@@ -0,0 +1,28 @@
+set -e
+
+if [ "$(docker ps -q | wc -l)" -gt 0 ]; then
+  echo "Containers are running"
+else
+  echo "No containers are running, run ./run-api-local.sh before this"
+  exit 1
+fi
+
+export VITESS_HOST=localhost
+export VITESS_PORT=3306
+export VITESS_DATABASE=entitybase
+export VITESS_USER=root
+export VITESS_PASSWORD=""
+export PYTHONPATH=src
+
+echo "Running E2E tests"
+#pytest -m integration
+
+# sdt out / logs
+#pytest -m integration -s --strict-markers
+
+# stop first failure
+#pytest -p no:xdist -m integration --exitfirst --capture=no --strict-markers
+pytest tests/e2e --capture=no --strict-markers --log-cli-level=DEBUG --log-cli-format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+
+# verbose
+#pytest -m integration -v --strict-markers
