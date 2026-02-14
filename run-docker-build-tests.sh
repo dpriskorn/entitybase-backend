1df6b78db415dcadfcad661ce9e67939eea47afc
Nizo Priskorn <68460690+dpriskorn@users.noreply.github.com>
2026-02-14 10:35:42 +0100

    Update statistics and docker test script

--- run-docker-build-tests.sh
+++ run-docker-build-tests.sh
@@ -3,7 +3,7 @@ set -e
 
 ./update-docs.sh
 #docker compose --file docker-compose.tests.yml down --remove-orphans -t 0 -v
-./run-docker-stop-and-remove-all.sh
+./stop-docker-and-remove-everything.sh
 ./clean-pyc.sh
 ./export-requirements.sh
 nice -20 docker compose --file docker-compose.tests.yml build
