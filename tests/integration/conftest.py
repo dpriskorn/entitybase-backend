5c1332f6e29207858cc1a27aac40fd45c88b41c0
Nizo Priskorn <68460690+dpriskorn@users.noreply.github.com>
2026-03-14 00:39:28 +0100

    Enable streaming for Elasticsearch integration tests
    - Fix Settings to load Kafka config from env vars in _load_streaming_config
    - Enable streaming by default in integration test conftest
    - Add Kafka config fixture to ES indexer tests

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -11,9 +11,11 @@ from botocore.exceptions import ClientError
 
 sys.path.insert(0, "src")
 
-os.environ["STREAMING_ENABLED"] = "false"
+os.environ["STREAMING_ENABLED"] = "true"
 if "KAFKA_BOOTSTRAP_SERVERS" not in os.environ:
     os.environ["KAFKA_BOOTSTRAP_SERVERS"] = "redpanda:9092"
+if "KAFKA_ENTITYCHANGE_JSON_TOPIC" not in os.environ:
+    os.environ["KAFKA_ENTITYCHANGE_JSON_TOPIC"] = "wikibase.entity_change"
 
 # noinspection PyPep8
 from models.config.settings import settings
